6bdcedd940efbf33c06520a7aebda97cd5f6bd36
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-07-05 19:46:40 -0300

    estructura de el practico de bucles

---
+++ js/bucles.js
@@ -0,0 +1,14 @@
+/* bucles
+
+while(condicion logica){
+    todo las lineas de codigo que quiero que repita mi programa
+    cambiar la condicion logica 
+}
+
+*/
+
+let cantidad=1;
+while(cantidad<=20){
+    document.write("<br>preparar mate y entregarlo al siguiente compañero,mate cebado n: "+cantidad);
+    cantidad=cantidad+1;
+}
