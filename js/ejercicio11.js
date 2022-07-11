60003b0c79b27a2dcb6f17698812daee7fd07e8e
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-07-11 20:37:26 -0300

    realice ejercicio numero 13

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -6,11 +6,11 @@ Nota: ver funcion Math() https://www.w3schools.com/js/js_math.asp
 
 */
 let nombre1=pront("¿cual es el nombre de la persona?")
-let edad1=parseInt(pront("¿Y cual es su edad?"))
+let edad1=Number(pront("¿Y cual es su edad?"))
 let nombre2=pront("¿cual es el nombre de la segunda persona?")
-let edad2=parseInt(prompt("¿y cual es su edad?"))
+let edad2=Number(prompt("¿y cual es su edad?"))
 let nombre3=pront("¿cual es el nombre de la tercera persona?")
-let edad3=parseInt(pront("¿y cual es su edad?"))
+let edad3=Number(pront("¿y cual es su edad?"))
 if(edad1>edad2&&edad1>edad3){
     document.write("El nombre del la persona con mayor edad es: "+(nombre1))
 }else if(edad2>edad1&&edad2>edad3){
