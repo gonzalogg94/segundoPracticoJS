6297b494a085ebf61cfe2988fdf9c5d3f7b65d20
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-07-13 23:18:05 -0300

    falta realizar ejejercicio 8 y 12

--- js/ejercicio17.js
+++ js/ejercicio17.js
@@ -1,3 +1,21 @@
 /*
 Realiza un script que muestre la posición de la primera vocal de un texto introducido por teclado.
 */
+    let texto = prompt("Introduce un texto");
+    let vocales = ["a", "e", "i", "o", "u"];
+    let oracion = texto.toLowerCase();
+    let posicion = 0;
+    let parar = false;
+    for (let i = 0; i < oracion.length; i++) {
+        for (let j = 0; j < vocales.length; j++) {
+            if (vocales[j] == oracion.charAt(i)) {
+                posicion = i;
+                parar = true;
+                break;
+            }
+        }
+        if (parar) {
+            break;
+        }
+    }
+    document.write("La primera vocal está en la posición " + posicion);
--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -8,7 +8,28 @@ Crea script para generar pirámide siguiente con los números del 1 al número q
 12345
 123456
 ……
-
-
-
 */
+
+//PEDIMOS EL NÚMERO POR TECLADO
+var numrep = prompt("Introduce numero de repeticiones");
+//SI LO INTRODUCIDO ES UN NÚMERO
+if (Number(numrep) == numrep) {
+    //SI ES UN NÚMERO ENTRE 1 Y 50
+    if (numrep > 0 && numrep <= 50) {
+        var rep,i;
+        //BUCLE QUE VA DESDE 0 HASTA numrep
+        for (i = 0; i <=numrep; i++) {
+            //BUCLE DESDE 1 HASTA i
+            for (rep = 1; rep <= i; rep++) {
+                document.write(rep);
+            }
+            document.write("<br>");
+        }
+    } 
+    else {
+        alert("El número introducido no es válido");
+    }
+} 
+else {
+    alert("No has introducido un número);
+}
